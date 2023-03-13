ec392c8270ee01ae9e9d46df2378121581c8c794
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-03-13 12:59:18 +0100

    started quiz app - practice project

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+import '../scss/style.scss'
+
+const quizData = [
+	{
+		question: 'Ile lat ma prezydent Duda?',
+		a: 'Prezydent ma 25 lat',
+		b: 'Prezydent ma 35 lat',
+		c: 'Prezydent ma 45 lat',
+		d: 'Prezydent ma 55 lat',
+		correct: 'd',
+	},
+	{
+		question: 'how you feel?',
+		a: '25',
+		b: '25',
+		c: '25',
+		d: '30',
+		correct: 'a',
+	},
+]
